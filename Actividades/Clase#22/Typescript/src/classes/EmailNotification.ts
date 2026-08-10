a178aae0f0d509a163b922bb81172fa71fc95c92
SebasHernandezG <shijiro433@gmail.com>
2026-08-10 11:23:20 -0600

    Feat: se realizaron las actividades de la clase 23

--- Actividades/Clase#22/Typescript/src/classes/EmailNotification.ts
+++ Actividades/Clase#22/Typescript/src/classes/EmailNotification.ts
@@ -1,15 +1,12 @@
-import BaseNotification from "./BaseNotification";
+import BaseNotification from './BaseNotification';
 
 export class EmailNotification extends BaseNotification {
-  constructor(
-    recipient: string,
-    message: string,
-    public readonly subject: string,
-  ) {
-    super(recipient, message);
-  }
-
-  sendNotification(): void {
-    this.logNotification("Email");
-  }
+    constructor(recipient: string, message: string,public readonly subject: string) {
+        super(recipient, message);
+ 
+    }
+    sendNotification(): void {
+        this.logNotification("Email");
+        console.log("Enviando Email de : ${this.recipient}con el asunto: ${this.subject} y el mensaje: ${this.message}");
+    }
 }
--- Actividades/Clase#22/Typescript/src/index.ts
+++ Actividades/Clase#22/Typescript/src/index.ts
@@ -1,3 +1,16 @@
-import {NotificationService} from "./service/NotificationService";
-import {EmailNotification} from "./classes/EmailNotification";
-import {SmsNotification} from "./classes/SmsNotification";
+import { EmailNotification } from "./classes/EmailNotification";
+import { SmsNotification } from "./classes/SmsNotification";
+import { NotificationService } from "./service/NotificationService";
+
+const notificationService = new NotificationService();
+
+const notifications = [
+  new EmailNotification(
+    "jeff@example.com",
+    "Bienvenido al sistema",
+    "Bienvenida",
+  ),
+  new SmsNotification("8888-8888", "Tu codigo de verificacion es 1234"),
+];
+
+notificationService.sendAll(notifications);
